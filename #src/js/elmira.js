8c5ff511c318f8aed06580906c5091984fb9fce0
Elmira <corpse.17@mail.ru>
2020-07-30 04:55:30 +0500

    office-contacts

--- #src/js/elmira.js
+++ #src/js/elmira.js
@@ -21,3 +21,34 @@ new DropDown(
     'payment-drop-down-trigger',
     'payment-changer'
 )
+
+
+ymaps.ready(function () {
+    var map = new ymaps.Map('map', {
+        center: [56.946262, 24.091404],
+        zoom: 1,
+        controls: []
+    });
+
+    var pin = ymaps.templateLayoutFactory.createClass('<img src="img/offices/pin.png" alt="pin">');
+    var office1 = new ymaps.Placemark(
+        [56.943325, 24.018319], {
+            hintContent: 'Anniņmuižas 17'
+        }, {
+            iconLayout: pin,
+        }
+    );
+    map.geoObjects.add(office1);
+
+    var office2 = new ymaps.Placemark(
+        [56.951233, 24.134147], {
+            hintContent: 'Ģertrudes 77'
+        }, {
+            iconLayout: pin,
+        }
+    );
+    map.geoObjects.add(office2);
+    map.panes.get('ground').getElement().style.filter = 'grayscale(100%)';
+});
+
+
